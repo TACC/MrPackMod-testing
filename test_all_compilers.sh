0ea0ef1bd5b71137d4cc7807b7ff047ee3b07de8
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-09-23 15:17:46 -0500

    test version number

--- test_all_compilers.sh
+++ test_all_compilers.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+pkgversion=$( mpm.py version )
+while [ $# -gt 0 ] ; do
+    if [ "$1" = "-v" ] ; then
+	shift && pkgversion=$1 && shift
+    else
+	echo "Error: unrecognized option <<$1>>" && exit 1
+    fi
+done
+
 package=$( mpm.py package | tail -n 1 )
 echo "================================================================"
-echo "    Testing package=${package}"
+echo "    Testing package=${package}/${pkgversion}"
 echo "================================================================"
 echo
 
@@ -31,13 +40,14 @@ for compiler in $( cat ../compilers_${host}.sh ) ; do
 	echo "Could not load compiler=${compiler}"
 	continue
     else
-	module -t try-load ${package} 2>/dev/null
+	module -t try-load ${package}/${pkgversion} 2>/dev/null
 	if [ $? -gt 0 ] ; then
 	    echo "Could not load package=${package}"
 	    continue
 	fi
 	compilermodule=$( module -t show ${compiler} 2>&1 )
 	SYSTEMMODULES=cmake \
+	     PACKAGEVERSION=${pkgversion} \
 	     REGRESSIONHEADEREXTRA=", compiler=${compilermodule}" \
 	     mpm.py regression
     fi
